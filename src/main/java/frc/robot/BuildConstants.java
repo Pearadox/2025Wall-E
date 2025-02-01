068cd09b4ea0ca79a4f102966a94d966acd3cd4c
nh17000 <46301909+nh17000@users.noreply.github.com>
2025-02-01 16:54:11 -0600

    Lower drive kP

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "2025Wall-E";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 9;
+    public static final String GIT_SHA = "7dca03dd1348967f1feb14d25c19f3ee15cb198f";
+    public static final String GIT_DATE = "2025-02-01 17:21:22 EST";
+    public static final String GIT_BRANCH = "maple_sim_talon_template";
+    public static final String BUILD_DATE = "2025-02-01 17:28:45 EST";
+    public static final long BUILD_UNIX_TIME = 1738448925685L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
